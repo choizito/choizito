8ec67192a38b859c00c5069d4c24f76c4f05533c
radproject <radhouanef@gmail.com>
2013-11-09 21:07:43 +0100

    user module

---
+++ api/models/User.js
@@ -0,0 +1,45 @@
+/**
+ * User
+ *
+ * @module      :: Model
+ * @description :: basic model for a choizito user object 
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+var bcrypt = require('bcrypt');
+module.exports = {
+
+  attributes: {
+	
+	name: {
+		type:'string',
+		required:'true'
+              },
+	mail: {
+		type:'email',
+		required:'true'
+              },
+	 password: {
+		type: 'string',
+		minLength: 6,
+		required: true,
+		columnName: 'encrypted_password'
+	      },
+	fbid:'string' ,
+	 // Override toJSON instance method
+	 // to remove password value
+	toJSON: function() {
+	  var obj = this.toObject();
+	   obj.password="HIDDEN"
+	  return obj;
+	   } 
+  },
+  // Encrypting pasword before saving it to database
+  beforeCreate: function(values, next) {
+    bcrypt.hash(values.password, 10, function(err, hash) {
+      if(err) return next(err);
+      values.password = hash;
+      next();
+    });
+  }
+};
